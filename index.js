93f7db9b46c0d945b579843b97d3af881cd3b2ef
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-01 19:00:28 +0700

    cai dat pug va tao folder views

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-    res.send("trang chu");
+    res.render("client/pages/home/index.pug")
 })
 
 app.get("/products", (req, res) => {
-    res.send("trang danh sach san pham");
+    res.render("client/pages/products/index.pug")
 })
 
 app.listen(port, () => {
