e5abcb92dc6801c5b84361e296f0b7a2e1515f2f
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-01 18:45:21 +0700

    Create Prject

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("trang chu");
+})
+
+app.get("/products", (req, res) => {
+    res.send("trang danh sach san pham");
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
