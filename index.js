1bf0492d4aec1ceb56e6b56a0adbd3d37b09e046
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-01 19:11:13 +0700

    cau hinh folder routes

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require("express");
+
+const routeClient = require("./routes/client/index.route")
 const app = express();
 const port = 3000;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
-    res.render("client/pages/home/index.pug")
-})
-
-app.get("/products", (req, res) => {
-    res.render("client/pages/products/index.pug")
-})
+routeClient.index(app);
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
-})
+});
