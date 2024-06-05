7ca4bebbcdaa8495f9d5037603effeee5609af6c
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-05 18:23:43 +0700

    nhung file tinh~

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const routeClient = require("./routes/client/index.route")
 const app = express();
 const port = process.env.PORT;
 
+
 app.set("views", "./views");
 app.set("view engine", "pug");
+
+app.use(express.static('public'));
 
 routeClient.index(app);
 
