0c72e1c6d1e550a01670b03388c38c25fed90a09
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-05 21:19:18 +0700

    Tao model

---
+++ models/product.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const Product = mongoose.model(
+   'Product', 
+    {
+        title: String,
+        description: String,
+        price: Number,
+        discountPercentage: Number,
+        stock: Number,
+        thumbnail: String,
+        position: Number,
+        deleted: Boolean,
+    }, 
+    "products");
+
+
+// Cach 2 dinh nghia 1 model
+
+// const productSchema = new Schema({
+//     title: String,
+//     description: String,
+//     price: Number,
+//     discountPercentage: Number,
+//     stock: Number,
+//     thumbnail: String,
+//     position: Number,
+//     deleted: Boolean,
+// });
+// const Product = mongoose.model("Product", productSchema, "products");
+
+module.exports = Product;
