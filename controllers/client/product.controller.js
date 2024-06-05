8877fdae4452b6f569076e7062ccd8c3ff13dd15
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-05 21:35:31 +0700

    lam trang danh sach san pham ben Client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,21 +1,21 @@
+const Product = require("../../models/product.model");
+
 // [GET] /products/
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+    
+    for(const item of products){
+        item.priceNew = ((1 - item.discountPercentage/100) * item.price).toFixed(0);
+    }
+     
+    console.log(products);
+
+    
     res.render("client/pages/products/index.pug", {
-        pageTitle: "Dang sach san pham"
+        pageTitle: "Dang sach san pham",
+        products: products 
     });
 }
-
-// [POST] /products/create
-// module.exports.create = (req, res) => {
-//   res.render("client/pages/products/create");
-// }
-
-// [PATCH] /products/edit
-// module.exports.edit = (req, res) => {
-//   res.render("client/pages/products/edit");
-// }
-
-// [GET] /products/detail
-// module.exports.detail = (req, res) => {
-//   res.render("client/pages/products/detail");
-// }
--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -4,10 +4,5 @@ const router = express.Router();
 const controller = require("../../controllers/client/product.controller")
 router.get("/", controller.index);
 
-// router.post("/create", controller.create);
-
-// router.patch("/edit", controller.edit);
-
-// router.get("/detail", controller.detail);
 
 module.exports = router;
