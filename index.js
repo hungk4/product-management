4020fe7f6647b7f5076da9b1a149701a472dc2b3
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-07 14:42:50 +0700

    tao trang tong quan

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require("express");
 require('dotenv').config();
-const mongoose = require('mongoose');
 
 const database = require("./config/database");
 database.connect();
 
-const routeClient = require("./routes/client/index.route")
+const routeAdmin = require("./routes/admin/index.route");
+const routeClient = require("./routes/client/index.route");
+
 const app = express();
 const port = process.env.PORT;
-
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(express.static('public'));
 
+routeAdmin.index(app);
 routeClient.index(app);
 
 app.listen(port, () => {
