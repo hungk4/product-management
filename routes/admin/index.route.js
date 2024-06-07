0a61732624e8cf5bcb85c05121b0d0c7a9e6d454
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-07 15:58:00 +0700

    Lam giao dien danh sach san pham

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -1,5 +1,6 @@
 const dashboardRoute = require("./dashboard.route");
-
+const productRoute = require("./product.route");
 module.exports.index = (app) => {
   app.use("/admin/dashboard", dashboardRoute);
+  app.use("/admin/products", productRoute);
 }
