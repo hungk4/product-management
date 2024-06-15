7fc21d91b7303c21e3d21a16b3532da24c4e0be5
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-15 14:18:20 +0700

    thay doi trang thai nhieu san pham - Backend nhan data va luu vao database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require('dotenv').config();
+const bodyParse = require('body-parser');
 
 const database = require("./config/database");
 database.connect();
@@ -7,9 +8,13 @@ database.connect();
 const routeAdmin = require("./routes/admin/index.route");
 const routeClient = require("./routes/client/index.route");
 const systemConfig = require("./config/system");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT;
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.set("views", "./views");
 app.set("view engine", "pug");
