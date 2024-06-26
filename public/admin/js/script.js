f592018d21b2001537c58eb5689288afd6792c07
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-06-26 23:22:29 +0700

    preview anh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -259,5 +259,20 @@ if(listButtonPermanentlyDelete.length > 0){
     })
   });
 }
-
 // End permanentlyDelete
+
+
+// Upload image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage){
+  const uploadImageInput = uploadImage.querySelector("[upload-image-input]");
+  const uploadImagePreview = uploadImage.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", () => {
+    const file = uploadImageInput.files[0];
+    if(file){
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  })
+}
+// End upload Image
