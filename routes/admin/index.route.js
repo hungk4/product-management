94b61882706557568771b6a0e8512be4b85adfb5
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-07-16 18:54:35 +0700

    xay dung phan Danh sach nhom quyen

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,18 +2,19 @@ const dashboardRoute = require("./dashboard.route");
 const productRoute = require("./product.route");
 const recycleBinRoute = require("./recycleBin.route");
 const productsCategoryRoute = require("./productCategory.route");
+const roleRoute = require("./role.route");
 const systemConfig = require("../../config/system");
 module.exports.index = (app) => {
 
   const path = `/${systemConfig.prefixAdmin}`;
-  // app.use("/admin/dashboard", dashboardRoute);
+
   app.use(`${path}/dashboard`, dashboardRoute);
   
-  // app.use("/admin/products", productRoute);
   app.use(`${path}/products`, productRoute);
 
-  // app.use("/admin/recycleBin", recycleBinRoute);
   app.use(`${path}/recycleBin`, recycleBinRoute);
 
   app.use(`${path}/products-category`, productsCategoryRoute);
+
+  app.use(`${path}/roles`, roleRoute);
 }
