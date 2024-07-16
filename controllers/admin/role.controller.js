14e13313481605532c2585cd8c401692ef306cef
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-07-16 21:21:49 +0700

    them moi nhom quyen

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -1,4 +1,5 @@
 const Role = require("../../models/role.model");
+const systemConfig = require("../../config/system");
 // [GET] /admin/roles
 module.exports.index = async (req, res) => {
   const records = await Role.find({
@@ -8,4 +9,19 @@ module.exports.index = async (req, res) => {
     pageTitle: "Nhóm quyền",
     records: records
   });
+}
+
+// [GET] /admin/roles/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/roles/create.pug", {
+    pageTitle: "Thêm mới nhóm quyền"
+  });
+}
+
+// [POST] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+    const records = new Role(req.body);
+    await records.save();
+
+    res.redirect(`/${systemConfig.prefixAdmin}/roles`);
 }
