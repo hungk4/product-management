d08ad6a47d2dd7e115934bd3adb6538a7303efa5
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-07-23 16:11:33 +0700

    trang ket qua tim kiem

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,6 +1,6 @@
 const homeRoute = require("./home.route");
 const productRoute = require("./product.route");
-
+const searchRoute = require("./search.route");
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 module.exports.index = (app) => {
     app.use(categoryMiddleware.category);
@@ -8,4 +8,6 @@ module.exports.index = (app) => {
     app.use("/", homeRoute);
 
     app.use("/products", productRoute);
+
+    app.use("/search", searchRoute)
 }
