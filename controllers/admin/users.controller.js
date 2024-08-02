1833eda532e3d61bc6517598055aa56a8f1711af
hungk4 <nguyenmanhhung15022004@gmail.com>
2024-08-02 15:16:57 +0700

    nut cap nhat trang thai trang quan ly tai khoan users

--- controllers/admin/users.controller.js
+++ controllers/admin/users.controller.js
@@ -1,9 +1,36 @@
 const User = require("../../models/user.model");
 
+
+// [GET] /admin/users
 module.exports.index = async (req, res) => {
   const records = await User.find({});
   res.render("admin/pages/users/index.pug", {
     pageTitle: "Quản lý tài khoản user",
     records: records
   })
+}
+
+// [PATCH] /admin/users/change-status/:statusChange/:id
+module.exports.changeStatus = async (req, res) => {
+  try{
+    if(res.locals.role.permissions.includes("products_edit")){
+      const { id, statusChange } = req.params;
+  
+      await User.updateOne({
+        _id: id
+      }, {
+        status: statusChange
+      });
+    
+      req.flash('success', 'cập nhật trạng thái thành công!');
+      
+      res.json({
+        code: 200
+      });
+    }else{
+      res.send("403");
+    }
+  } catch(error) {
+    req.flash("error", "id sản phẩm không hợp lệ!")
+}
 }
--- routes/admin/users.route.js
+++ routes/admin/users.route.js
@@ -4,4 +4,6 @@ const controller = require("../../controllers/admin/users.controller");
 
 router.get("/", controller.index);
 
+router.patch("/change-status/:statusChange/:id", controller.changeStatus);
+
 module.exports = router;
